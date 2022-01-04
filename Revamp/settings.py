5764f7391f5113b0aeeb7de800eef961850792f7
IM <85082822+molin6@users.noreply.github.com>
2022-01-04 13:23:21 -0600

    Update settings.py

--- Revamp/settings.py
+++ Revamp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ouo0jzitbh^pg#lwj00ji1la3w2dvn72gqk5%*pu(a@=n)05-5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
